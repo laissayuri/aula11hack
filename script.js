35674d44b3f33988e62caa98a5cf01299756c1bd
laissayuri <k.lais.sayuri@gmail.com>
2018-08-20 08:51:11 -0300

    Entrada e Saída de Dados

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite Aqui Seu Nome:"); //pede o nome
+
+console.log(`O nome digitado foi ${nome}`); //imprime o nome no console
+
+alert(`Seja Bem vindo, ${nome}! Aproveite sua visita`); //aparece o alert com a mensagem
